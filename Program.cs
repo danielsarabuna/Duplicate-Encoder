04f7aec114b571326e470df87535ac555fe4d479
danielsarabuna <danielsarabuna@gmail.com>
2022-01-09 00:13:31 +0200

    v1.1

--- Program.cs
+++ Program.cs
@@ -21,10 +21,10 @@ public class Program
 
     for (int i = 0; i < arr.Length; i++)
     {
-        int countChar = 0;
-        for (int k = 0; k < arr.Length; k++)
-            if (Char.ToUpper(arr[i]) == Char.ToUpper(arr[k])) countChar++;
-        result += countChar <= 1 ? "(" : ")";
+      int countChar = 0;
+      for (int k = 0; k < arr.Length; k++)
+        if (Char.ToUpper(arr[i]) == Char.ToUpper(arr[k])) countChar++;
+      result += countChar <= 1 ? "(" : ")";
     }
 
     return result;
